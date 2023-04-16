03017442d5cd53b8a6fc75ff56b1907405f2d3aa
Art-St <tatsuhado93@gmail.com>
2023-04-15 20:19:03 -0600

    This fixes the I block issue with one of its blocks being misplaced

--- Blocks/IBlock.cs
+++ Blocks/IBlock.cs
@@ -7,10 +7,10 @@
             new Position[] {new(1,0), new(1,1), new(1,2), new(1,3) },
             new Position[] {new(0,2), new(1,2), new(2,2), new(3,2) },
             new Position[] {new(2,0), new(2,1), new(2,2), new(2,3) },
-            new Position[] {new(0,1), new(1,1), new(2,2), new(3,1) }
+            new Position[] {new(0,1), new(1,1), new(2,1), new(3,1) }
         };
         public override int _id => 1;
-        protected override Position _startOffset => new Position(-1, 3);
+        protected override Position _startOffset => new(-1, 3);
         protected override Position[][] _tiles => _tileStates;
     }
 }
--- MainFunctions/PlayfieldState.cs
+++ MainFunctions/PlayfieldState.cs
@@ -3,19 +3,19 @@
     public class PlayfieldState
     {
         public Blocks _currentBlock;
-        public Blocks currentBlock
+        public Blocks CurrentBlock
         {
             get => _currentBlock;
             private set
             {
                 _currentBlock = value;
-                currentBlock.Reset();
+                CurrentBlock.Reset();
 
                 for (int i = 0; i < 2; i++)
                 {
-                    currentBlock.Move(1, 0);
+                    CurrentBlock.Move(1, 0);
                     if (!BlockFits())
-                        currentBlock.Move(-1, 0);
+                        CurrentBlock.Move(-1, 0);
                 }
             }
         }
@@ -29,12 +29,12 @@
         {
             _playfield = new Playfield(22, 10);
             _blockQueue = new BlockQueue();
-            currentBlock = _blockQueue.RerollBlock();
+            CurrentBlock = _blockQueue.RerollBlock();
             _canHold = true;
         }
         private bool BlockFits()
         {
-            foreach (Position position in currentBlock.TilePositions())
+            foreach (Position position in CurrentBlock.TilePositions())
             {
                 if (!_playfield.IsEmpty(position._row, position._column))
                     return false;
@@ -47,41 +47,41 @@
                 return;
             if(_heldBlock == null)
             {
-                _heldBlock = currentBlock;
-                currentBlock = _blockQueue.RerollBlock();
+                _heldBlock = CurrentBlock;
+                CurrentBlock = _blockQueue.RerollBlock();
             }
             else
             {
-                Blocks temporaryBlock = currentBlock;
-                currentBlock = _heldBlock;
+                Blocks temporaryBlock = CurrentBlock;
+                CurrentBlock = _heldBlock;
                 _heldBlock = temporaryBlock;
             }
             _canHold = false;
         }
         public void RotateBlockRight()
         {
-            currentBlock.RotateRight();
+            CurrentBlock.RotateRight();
             if (BlockFits())
-                currentBlock.RotateLeft();
+                CurrentBlock.RotateLeft();
 
         }
         public void RotateBlockLeft()
         {
-            currentBlock.RotateLeft();
+            CurrentBlock.RotateLeft();
             if (!BlockFits())
-                currentBlock.RotateRight();
+                CurrentBlock.RotateRight();
         }
         public void MoveBlockRight()
         {
-            currentBlock.Move(0, 1);
+            CurrentBlock.Move(0, 1);
             if (!BlockFits())
-                currentBlock.Move(0, -1);
+                CurrentBlock.Move(0, -1);
         }
         public void MoveBlockLeft()
         {
-            currentBlock.Move(0, -1);
+            CurrentBlock.Move(0, -1);
             if (!BlockFits())
-                currentBlock.Move(0, 1);
+                CurrentBlock.Move(0, 1);
         }
         private bool IsGameOver()
         {
@@ -89,23 +89,23 @@
         }
         private void PlaceBlock()
         {
-            foreach (Position position in currentBlock.TilePositions())
-                _playfield[position._row, position._column] = currentBlock._id;
+            foreach (Position position in CurrentBlock.TilePositions())
+                _playfield[position._row, position._column] = CurrentBlock._id;
             _score += _playfield.ClearFullRows();
             if (IsGameOver())
                 _gameOver = true;
             else
             {
-                currentBlock = _blockQueue.RerollBlock();
+                CurrentBlock = _blockQueue.RerollBlock();
                 _canHold = true;
             }
         }
         public void MoveBlockDown()
         {
-            currentBlock.Move(1, 0);
+            CurrentBlock.Move(1, 0);
             if (!BlockFits())
             {
-                currentBlock.Move(-1, 0);
+                CurrentBlock.Move(-1, 0);
                 PlaceBlock();
             }
         }
@@ -124,7 +124,7 @@
         {
             int drop = _playfield._rows;
 
-            foreach (Position position in currentBlock.TilePositions())
+            foreach (Position position in CurrentBlock.TilePositions())
             {
                 drop = System.Math.Min(drop, TileDropDistance(position));
             }
@@ -132,7 +132,7 @@
         }
         public void DropBlock()
         {
-            currentBlock.Move(BlockDropDistance(), 0);
+            CurrentBlock.Move(BlockDropDistance(), 0);
             PlaceBlock();
         }
     }
